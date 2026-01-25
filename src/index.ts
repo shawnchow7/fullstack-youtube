aa8e285004c9b86983545ab7b0fd3517cf8e6403
ShawnChow <132461800+shawnchow7@users.noreply.github.com>
2026-01-25 19:35:40 +0800

    first inital commit, setting up project

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (request,response) => {
+    response.send('hello world')
+})
+
+
+app.listen(port, ()=> {
+    console.log(`app is listening on port: ${port}`)
+})
